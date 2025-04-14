69e59d03bcaf87d084297ea9d3a54bfa9c8d302d
Omar Ahmed <abdi.omar@riarauniversity.ac.ke>
2025-04-15 01:18:34 +0300

    Day three

--- rust/day_two/src/main.rs
+++ rust/day_two/src/main.rs
@@ -1,6 +1,11 @@
 // Day 3: Exploring Rust functions
 fn main() {
+    let arr  = [1,2,3,5];
+    let tupl = ("Omar", 2, "AHmed");
     println!("Hello, world!");
+
+    println!("array : {:?}", arr);
+    println!("Tuple : {:?}", tupl);
     let result = add(4, 7);
     println!("Result is = {}", result);
     greeting("Omar", 1);
@@ -13,3 +18,4 @@ fn add(a: i32, b: i32) -> i32 {
 fn greeting(name: &str, age: i32) {
     println!("Hello {}!, you are {} years old", name, age);
 }
+
